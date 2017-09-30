24d92fb00f8f6dafc661a04255eaefb3756a7ccc
David Rhodus <david@vacovideo.com>
2017-09-29 18:18:21 -0700

    formatting

--- one.cpp
+++ one.cpp
@@ -18,9 +18,9 @@ class Neuron
         return x;
     }
     // ReLU activation function
-    float getActReLU(const float& x ) {
+    float getActReLU(const float &x)
+    {
         return std::max(0.0f, x);
-
     }
     float propForward(const float &x)
     {
@@ -42,25 +42,26 @@ class Neuron
 
 int main()
 {
-    std::cout << "Hello, Neuron!\n" << std::endl;
+    std::cout << "Hello, Neuron!\n"
+              << std::endl;
 
     Neuron my_neuron;
     // Linear relation between input and output
-    std::cout << "Linear forward prop" << std::endl;    
-    std::cout << my_neuron.propForward(-1.0) << std::endl;    
-    std::cout << my_neuron.propForward(-0.5) << std::endl;        
+    std::cout << "Linear forward prop" << std::endl;
+    std::cout << my_neuron.propForward(-1.0) << std::endl;
+    std::cout << my_neuron.propForward(-0.5) << std::endl;
     std::cout << my_neuron.propForward(0.0) << std::endl;
     std::cout << my_neuron.propForward(0.5) << std::endl;
     std::cout << my_neuron.propForward(1.0) << std::endl;
     std::cout << my_neuron.propForward(2.0) << std::endl;
-    
-    std::cout << "RelU forward prop" << std::endl;      
+
+    std::cout << "RelU forward prop" << std::endl;
     std::cout << my_neuron.propForwardReLU(-1.0) << std::endl;
-    std::cout << my_neuron.propForwardReLU(-0.5) << std::endl;    
+    std::cout << my_neuron.propForwardReLU(-0.5) << std::endl;
     std::cout << my_neuron.propForwardReLU(0.0) << std::endl;
     std::cout << my_neuron.propForwardReLU(0.5) << std::endl;
     std::cout << my_neuron.propForwardReLU(1.0) << std::endl;
     std::cout << my_neuron.propForwardReLU(2.0) << std::endl;
-
+    
     return 0;
 }
